aadc774a249b3d47dcbe30965981812d72639e82
diguinddtank-code <diguin.ddtank@gmail.com>
2026-05-15 11:54:21 -0300

    feat(coaches): Add Alex Deus and adjust grid layout
    Adds a new coach, Alex Deus, to the coaches grid and adjusts the grid to accommodate more coaches by increasing the number of columns on larger screens from 3 to 4.

--- components/CoachesGrid.tsx
+++ components/CoachesGrid.tsx
@@ -28,6 +28,11 @@ export default function CoachesGrid() {
       role: 'Licensed Staff',
       photo: 'https://i.imgur.com/trDxKdC.png',
     },
+    {
+      name: 'Alex Deus',
+      role: 'Licensed Staff',
+      photo: 'https://i.imgur.com/ukBLp7N.png',
+    },
   ];
 
   return (
@@ -103,7 +108,7 @@ export default function CoachesGrid() {
           </motion.div>
 
           {/* Staff Grid */}
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 sm:gap-6">
             {staffCoaches.map((coach, i) => (
               <motion.div
                 key={i}
